379f75b3ff124010830ae6cd9bf76d36296b73c4
renko7 <66572823+renko7@users.noreply.github.com>
2022-06-20 10:25:47 -0400

    meta-data for root component creation

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+// takes in meta-data with known properties to make class an angular component
+@Component({
+    selector: 'app-root',
+    template: '<h1>My App</h1>'
+})
+export class AppComponent{}
