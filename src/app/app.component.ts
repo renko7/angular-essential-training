9653c40d011a82431d01bccc5af901c3d21819ab
renko7 <66572823+renko7@users.noreply.github.com>
2022-06-20 10:35:38 -0400

    renaming custom tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 // takes in meta-data with known properties to make class an angular component
+// W3 standard says to have a prefix for all custom DOM properties
 @Component({
-    selector: 'app-root',
+    selector: 'mw-app',
     template: '<h1>My App</h1>'
 })
 export class AppComponent{}
