07c602d2b51d5715e13f6b9059b95bb8b9ded8f4
renko7 <66572823+renko7@users.noreply.github.com>
2022-06-21 10:05:59 -0400

    creating custom attribute directives. here once the attribute "mwFavorite" is added the "is-favorite" class is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   imports: [
@@ -11,8 +12,9 @@ import { MediaItemListComponent } from './media-item-list.component';
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
-  ],
+    MediaItemListComponent,
+    FavoriteDirective
+  ], //components directives and pipes that you want to be made avaible to others in your module
   bootstrap: [
     AppComponent
   ]
